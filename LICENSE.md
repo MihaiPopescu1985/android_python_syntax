14256630316347af65cc44ce5296b081c9212400
Mihai <justmaryo@gmail.com>
2023-02-06 18:59:23 +0200

    v1.0.0

---
+++ LICENSE.md
@@ -0,0 +1,2 @@
+This software is under Mozilla Public License Version 2.0 license.  
+You must consult the license before using this software:  [Mozilla Public License](https://searchfox.org/mozilla-central/source/mobile/android/LICENSE)  
--- app/src/main/java/com/wowotraining/python_syntax/MainActivity.java
+++ app/src/main/java/com/wowotraining/python_syntax/MainActivity.java
@@ -1,14 +1,36 @@
 package com.wowotraining.python_syntax;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static GeckoRuntime sRuntime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.showExercises();
+    }
+
+    private void showExercises() {
+        GeckoView view = this.findViewById(R.id.geckoview);
+        GeckoSession session = new GeckoSession();
+        //session.setContentDelegate(new GeckoSession.ContentDelegate() {});
+        if (sRuntime == null) {
+            //GeckoRuntime can only be initialized once per process
+            sRuntime = GeckoRuntime.create(this);
+        }
+        session.open(sRuntime);
+        view.setSession(session);
+        String TUTORIAL_URL = "https://mihaipopescu1985.github.io/wowotraining.com/python_syntax.html";
+        session.loadUri(TUTORIAL_URL);
     }
 }
